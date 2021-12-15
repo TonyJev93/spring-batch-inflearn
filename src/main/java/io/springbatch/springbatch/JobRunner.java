eaf1be99da4c40985becf01dcdfb345a23348f61
JUN GYEWON <tonyjev93@gmail.com>
2021-12-15 11:40:01 +0900

    feat: Job Instance & Job Parameter 테스트

---
+++ src/main/java/io/springbatch/springbatch/JobRunner.java
@@ -0,0 +1,33 @@
+package io.springbatch.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+// ApplicationRunner : SpringBoot 가 초기화되면 호출되는 기본 러너
+@Component
+public class JobRunner implements ApplicationRunner {
+
+    private final JobLauncher jobLauncher; // SpringBatch 초기화 시 Bean 으로 등록 됨.
+    private final Job simpleJob;
+
+    public JobRunner(JobLauncher jobLauncher, Job simpleJob) {
+        this.jobLauncher = jobLauncher;
+        this.simpleJob = simpleJob;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("name", "user2")
+                .addLong("age", 20L)
+                .toJobParameters();
+
+        jobLauncher.run(simpleJob, jobParameters);
+    }
+}
